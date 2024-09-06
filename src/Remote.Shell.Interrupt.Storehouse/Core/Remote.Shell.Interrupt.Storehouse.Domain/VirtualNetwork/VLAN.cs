e115ff04342907514171666001290ccac9490857
Strauberg <StefanStrauberg@gmail.com>
2024-09-06 16:13:45 +0300

    working 15.2

--- src/Remote.Shell.Interrupt.Storehouse/Core/Remote.Shell.Interrupt.Storehouse.Domain/VirtualNetwork/VLAN.cs
+++ src/Remote.Shell.Interrupt.Storehouse/Core/Remote.Shell.Interrupt.Storehouse.Domain/VirtualNetwork/VLAN.cs
@@ -3,7 +3,7 @@ public class VLAN : BaseEntity
 {
   public int VLANNumber { get; set; } // 10
   public string VLANName { get; set; } = string.Empty; // "VLAN10"
-
+  public IDictionary<string, int>? IPTable { get; set; }
   public ICollection<Port> InterfacesOfVLAN { get; set; } = [];
   public Guid NetworkDeviceId { get; set; }
 }
