22db5270c9bff3eef31cf1acdf53c126936d316c
Ruslan Stefanovich <stefanstrauberg@gmail.com>
2026-09-06 00:34:59 +0300

    fixed bugs on frontend

--- client/src/lib/api/tfPlans/TfPlansResponse.ts
+++
@@ -1,7 +0,0 @@
-import { PaginationMetadata } from "../../types/Common/PaginationMetadata";
-import { TfPlan } from "../../types/TfPlans/TfPlan";
-
-export type TfPlansResponse = {
-  data: TfPlan[];
-  pagination: PaginationMetadata;
-};
